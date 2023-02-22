6e218f310193071076f582039e132ed820603b6f
Wangyuhang <1219503210@qq.com>
2023-02-22 15:53:23 +0800

    完成分类mybatis映射

--- src/main/java/com/ms/blog/dao/CategoryMapper.java
+++ src/main/java/com/ms/blog/dao/CategoryMapper.java
@@ -19,9 +19,24 @@ public interface CategoryMapper {
      */
     Category getCategoryById(Integer id);
 
+    /**
+     * 通过分类名查找分类
+     * @param name 分类名
+     * @return 结果
+     */
     Category getCategoryByName(String name);
 
+    /**
+     * 查找所有分类
+     * @return 结果
+     */
     List<Category> getCategoryList();
+
+    /**
+     * 删除分类
+     * @param id 分类id
+     * @return 结果
+     */
     int deleteCategory(Integer id);
 
     /**
@@ -30,6 +45,19 @@ public interface CategoryMapper {
      * @return 结果
      */
     int insertCategory(Category category);
+
+    /**
+     * 更改分类
+     * @param category 分类
+     * @return 结果
+     */
     int updateCategory(Category category);
+
+    /**
+     * 更新分类点击量
+     * @param id    分类id
+     * @param clickVolume   点击量
+     * @return 结果
+     */
     int updateCategoryClickVolume(Integer id, Integer clickVolume);
 }
