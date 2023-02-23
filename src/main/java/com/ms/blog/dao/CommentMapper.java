a0f88b0b8d3e7a041f7155591a2a16a3af2993b6
Wangyuhang <1219503210@qq.com>
2023-02-23 14:54:46 +0800

    完成部分评论mybatis映射

--- src/main/java/com/ms/blog/dao/CommentMapper.java
+++ src/main/java/com/ms/blog/dao/CommentMapper.java
@@ -13,12 +13,40 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface CommentMapper {
 
+    /**
+     * 获取父评论
+     * @param articleId 文章id
+     * @param page 分页参数
+     * @return 结果
+     */
     IPage<Comment> getParentCommentList(Integer articleId, Page<Comment> page);
+
+    /**
+     * 获取子评论
+     * @param parentId 父评论id
+     * @param page 分页参数
+     * @return 结果
+     */
     IPage<Comment> getChildCommentList(Integer parentId, Page<Comment> page);
 
+    /**
+     * 删除评论
+     * @param id 评论id
+     * @return  删除评论
+     */
     int deleteComment(Integer id);
 
+    /**
+     * 插入子评论
+     * @param comment 评论类
+     * @return 结果
+     */
     int insertChildComment(Comment comment);
 
+    /**
+     * 插入父评论
+     * @param comment 评论类
+     * @return 结果
+     */
     int insertParentComment(Comment comment);
 }
