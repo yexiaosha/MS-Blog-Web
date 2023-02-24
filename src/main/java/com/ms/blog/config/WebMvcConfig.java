3f940d2de99416d64d14b5c010c51a6cee75a4d3
Wangyuhang <1219503210@qq.com>
2023-02-24 14:39:21 +0800

    修改拦截器配置

--- src/main/java/com/ms/blog/config/WebMvcConfig.java
+++ src/main/java/com/ms/blog/config/WebMvcConfig.java
@@ -4,8 +4,6 @@ import com.ms.blog.common.intercepter.AuthorizationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -28,20 +26,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
      * 拦截器配置
      * @param registry 注册
      */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        InterceptorRegistration registration = registry.addInterceptor(getUserLoginInterceptor());
-        registration.addPathPatterns("/**");
-        registration.excludePathPatterns(
-                "/msblog/user/login",
-                "/msblog/user/register",
-                "/msblog/captcha/**",
-                "/msblog/mail/**",
-                "/msblog/links/**",
-                "/msblog/**",
-                "/swagger**/**",
-                "/webjars/**",
-                "/v3/**",
-                "/doc.html");
-    }
+    // @Override
+    // public void addInterceptors(InterceptorRegistry registry) {
+    //     InterceptorRegistration registration = registry.addInterceptor(getUserLoginInterceptor());
+    //     registration.addPathPatterns("/**");
+    //     registration.excludePathPatterns(
+    //             "/msblog/user/login",
+    //             "/msblog/user/register",
+    //             "/msblog/captcha/**",
+    //             "/msblog/mail/**",
+    //             "/msblog/links/**",
+    //             "/msblog/**",
+    //             "/swagger**/**",
+    //             "/webjars/**",
+    //             "/v3/**",
+    //             "/doc.html");
+    // }
 }
